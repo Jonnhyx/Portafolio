debacf07608540390f13b8fe3ef4e0a3d9c5723a
Jonatan <34280963+Jonnhyx@users.noreply.github.com>
2020-11-16 13:23:34 +0100

    Update php-email-form.php

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -29,8 +29,6 @@ class Email_Form {
 	}
 
     function send() {
-
-		//mail($this->to,$this->subject,$this->message,$this->from_email);
 		$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
 		$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
