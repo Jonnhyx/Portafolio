502a9911b564c48b120cd10907734e8367b05573
Jonatan <34280963+Jonnhyx@users.noreply.github.com>
2020-11-16 13:21:53 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,24 +1,19 @@
 <?php
 
-  $receiving_email_address = 'jonnhyxdev@gmail.com';
+ $receiving_email_address = "jonnhyxdev@gmail.com";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  $php_email_form = "../assets/vendor/php-email-form/php-email-form.php";
+  include($php_email_form);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+
+  $contact = new Email_Form;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
+  $contact->setTo($receiving_email_address);
+  $contact->setFrom_name($_POST['name']);
+  $contact->setfrom_email($_POST['email']);
+  $contact->setsubject($_POST['subject']);
+  $contact->setmessage($_POST['message']);
+  
   echo $contact->send();
+
 ?>
